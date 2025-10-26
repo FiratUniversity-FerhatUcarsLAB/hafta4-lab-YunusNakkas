1c3713c1aa3c150023050288585f976380d5e5ac
YunusNakkas <101041495+YunusNakkas@users.noreply.github.com>
2025-10-26 16:14:07 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,29 @@
+/*
+* Ad Soyad: [Yunus Emre Nakkaş]
+* Ogrenci No: [250542021]
+* Tarih: [26/10/2025]
+* Aciklama: [Geometrikhesap]
+*/
 
+
+Scanner scanner= new Scanner(System.in);
+		System.out.println("Lütfen yarıçapı giriniz");
+		double yarıcap=scanner.nextDouble();
+		final  double pi = 3.14159;
+        double alan=pi*yarıcap*yarıcap;
+        double cevre=2*pi*yarıcap;
+        double hacim=4.0/3.0*pi*yarıcap*yarıcap*yarıcap;
+        double yuzey_alanı=4*pi*yarıcap*yarıcap;
+        
+		
+		System.out.println("--------Sonuçlar--------");
+		System.out.printf(" Diarenin Alanı: %.2f \n",alan );
+		System.out.printf(" Diarenin Çevresi: %.2f \n",cevre );
+		System.out.printf("Diarenin Çapı: %.2f \n",yarıcap );
+		System.out.printf("Diarenin Hacmi : %.2f \n",hacim );
+		System.out.printf("Diarenin Yüzey Alanı: %.2f ",yuzey_alanı );
+		
+		
+		
+		
+        scanner.close();
